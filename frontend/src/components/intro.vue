b78689e9d95e0da89d3a49086db63f08c1917707
刘彦秀 <lyx16280@163.com>
2018-08-13 16:01:53 +0800

    Add picture to free course frame. Ref #4

---
+++ frontend/src/components/intro.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="body">
+    <div style="text-align: center; margin:0px; ">
+      <img alt="" src="../assets/class1.jpg" width="700" height="300"/>
+    </div>
+  </div>
+</temolate>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    handleChange(value) {
+      console.log(value)
+    }
+  }
+}
+</script>
+<style>
+<style>
