48d21724f007aa3e6e24512e2961c3e75b2606a1
战瑀璠 <1612943@mail.nankai.edu.cn>
2018-08-14 20:54:03 +0800

    Add toolbar in page freelist and add routes

--- frontend/src/components/freelistpage.vue
+++ frontend/src/components/freelistpage.vue
@@ -49,4 +49,16 @@ export default {
     padding: 0;
   }
 
+  .toolbar {
+    width: 100%;
+    height: 55px;
+    margin: 0;
+    padding: 0;
+    text-align: right;
+    background-color:lightskyblue;
+    background: linear-gradient(lightskyblue, white);
+    opacity: 0.7;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
 </style>
