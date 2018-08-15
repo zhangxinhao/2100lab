6e36a7b4a29cd2e79b0ccb7e0e8f94066b7dacda
丁一峰 <879223669@qq.com>
2018-08-15 12:53:29 +0000

    Add method of commend courses. Ref #59

--- server/backend/urls.py
+++ server/backend/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index)
+  path('', views.index)
 ]
--- server/backend/views.py
+++ server/backend/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
+import json
 
 # Create your views here.
 def index(request):
     response = HttpResponse("sean")
     return response
+
+def recommendCourse(request):
+  list = Course.objects.filter().order_by('-create_time')
+  courses = []
+  for i in range(5):
+    courses.append(list[i])
+  return HttpResponse(json.dumps({"courses": courses}))
