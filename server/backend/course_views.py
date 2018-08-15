97aaa6d620873e30b3884cb454800d480bed93cf
丁一峰 <879223669@qq.com>
2018-08-15 17:56:44 +0000

    Add method to load course. Ref #60

---
+++ server/backend/course_views.py
@@ -0,0 +1,14 @@
+from .models import User, Course, Picture, Audio
+from .message_views import messageBoardDic
+from django.http import HttpResponse
+from django.contrib import auth
+import json
+
+def loadCourse(request):
+  course_id = request.POST.get("course_id")
+  course = Course.objects.get(pk=course_id)
+  pictures = Picture.objects.filter(course=course).order_by("start")
+  audio = course.audio_url
+  mb = messageBoardDic(request)
+  course = {"pictures": pictures, "audio": audio, "message": mb["message"], "comment": mb["comment"]}
+  return HttpResponse(json.dumps(course))
