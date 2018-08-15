69d9db99a6862589b6a644e0c9a2c4a9c136140a
丁一峰 <879223669@qq.com>
2018-08-15 13:42:42 +0000

    Add methods to list free and priced courses. Ref #59

--- server/backend/views.py
+++ server/backend/views.py
@@ -5,12 +5,40 @@ import json
 
 # Create your views here.
 def index(request):
-    response = HttpResponse("sean")
-    return response
+  response = HttpResponse("sean")
+  return response
 
 def recommendCourse(request):
   list = Course.objects.filter().order_by('-create_time')
   courses = []
   for i in range(5):
     courses.append(list[i])
+  return HttpResponse(json.dumps({"courses": courses}))
+
+def freeCourses(request):
+  return listCourses(request, True)
+
+def pricedCourses(request):
+  return listCourses(request, False)
+
+def briefFreeCourses(request):
+  return listCourses(request, True, 5)
+
+def briefPricedCourses(request):
+  return listCourses(request, False, 5)
+
+def listCourses(request, free=None, number=None):
+  list = []
+  if free is None:
+    list = Course.objects.filter().order_by("-create_time")
+  elif free is True:
+    list = Course.objects.filter(price=0).order_by("-create_time")
+  else:
+    Course.objects.filter(price_gt=0).order_by("-create_time")
+  courses = []
+  if number is not None:
+    for i in range(number):
+      courses.append(list[i])
+  else:
+    courses = list
   return HttpResponse(json.dumps({"courses": courses}))
