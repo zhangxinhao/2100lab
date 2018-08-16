602d492ccedb960b97c41ba20d48f34df1e45455
zhangxinhao <inkusean@gmail.com>
2018-08-16 01:49:38 +0000

    Send SMS. Ref #73

--- server/backend/models.py
+++ server/backend/models.py
@@ -40,7 +40,7 @@ class Course(models.Model):
 class Picture(models.Model):
   course = models.ForeignKey('Course', on_delete=models.CASCADE)
   postion = models.CharField(max_length=150)
-  start = PositiveIntegerField(default=0)
+  start = models.PositiveIntegerField(default=0)
 
 class Visit_record(models.Model):
   course = models.ForeignKey('Course', on_delete=models.CASCADE)
