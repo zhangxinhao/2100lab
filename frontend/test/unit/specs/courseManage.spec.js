78252c66672b2aaa5181f6fe3ccdf8cf0211f9f0
战瑀璠 <1612943@mail.nankai.edu.cn>
2018-08-19 20:02:25 +0800

    Add test in courseManage page. Ref #112

---
+++ frontend/test/unit/specs/courseManage.spec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import courseManage from '@/components/admin/courseManage'
+
+describe('courseManage', () => {
+  it('set a correct data', () => {
+    expect(typeof courseManage.data).toBe('function')
+    const defaultData = courseManage.data()
+    expect(defaultData.courseData[1].courseId).toBe(15223)
+    expect(defaultData.courseData[0].courseTitle).toBe('我们爱科学')
+  })
+})
