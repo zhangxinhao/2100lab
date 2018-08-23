f62968c0ac755f9eb9c4c09e72011fb07a0b74ff
丁一峰 <879223669@qq.com>
2018-08-23 09:55:02 +0000

    Show admin history. Ref #179

--- frontend/src/components/admin/adminHistory.vue
+++ frontend/src/components/admin/adminHistory.vue
@@ -5,7 +5,7 @@
     <el-row :gutter="20">
       <el-col :span="6" :offset="7"><el-input v-model="search_adminId" placeholder="请输入管理员编号"></el-input></el-col>
       <el-col :span="6" :offset="1"><el-input v-model="search_objectId" placeholder="请输入对象编号"></el-input></el-col>
-      <el-col :span="2"><el-button type="primary" icon="el-icon-search">搜索</el-button></el-col>
+      <el-col :span="2"><el-button type="primary" icon="el-icon-search" @click="getList">搜索</el-button></el-col>
     </el-row>
   </div>
 
@@ -13,10 +13,9 @@
     <el-table :data="historyData" border width=100%>
       <el-table-column type="index" :index="indexMethod" header-align=center></el-table-column>
       <el-table-column prop="adminId" label="管理员编号" width="150" header-align=center></el-table-column>
-      <el-table-column prop="operator" label="操作" width="150" header-align=center></el-table-column>
-      <el-table-column prop="objectName" label="操作对象" width="150" header-align=center></el-table-column>
+      <el-table-column prop="operation" label="操作" width="150" header-align=center></el-table-column>
       <el-table-column prop="objectId" label="对象编号" width="150" header-align=center></el-table-column>
-      <el-table-column prop="historyTime" label="操作时间" header-align=center></el-table-column>
+      <el-table-column prop="time" label="操作时间" header-align=center></el-table-column>
     </el-table>
   </div>
 
@@ -24,21 +23,40 @@
 </template>
 
 <script>
+import * as utils from '../utils/utils.js'
+import axios from 'axios'
+import qs from 'qs'
+
 export default {
   data() {
     return {
       search_adminId: '',
       search_objectId: '',
       historyData: [
-        {adminId: '001', operator: '添加课程', objectName: '我们爱学习', objectId: '15223', historyTime: '2018.08.20'},
-        {adminId: '002', operator: '删除用户', objectName: 'zzgyy', objectId: '15222681355', historyTime: '2018.08.19'}
+        {adminId: '001', operation: '添加课程', objectId: '15223', time: '2018.08.20'},
+        {adminId: '002', operation: '删除用户', objectId: '15222681355', time: '2018.08.19'}
       ]
     }
   },
   methods: {
     indexMethod(index) {
       return index + 1
+    },
+    getList() {
+      axios.post(utils.getURL() + 'api/recordlist/', qs.stringify({
+        adminId: this.search_adminId,
+        objectId: this.search_objectId
+      })).then(response => {
+        if (response.data.status === 0) {
+          this.historyData = response.data.history
+        } else {
+          alert('内部错误')
+        }
+      })
     }
+  },
+  created: function() {
+    this.getList()
   }
 }
 </script>
