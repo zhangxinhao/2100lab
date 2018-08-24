a5fa68fb31a6d82cd159a143af9f9b603a916f5a
zhangxinhao <inkusean@gmail.com>
2018-08-24 04:43:13 +0000

    Backend unittest: course. Ref #137

--- server/backend/tests/test_order.py
+++ server/backend/tests/test_order.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 from backend.models import Order, User, Course
 
 # Create your tests here.
@@ -29,9 +29,21 @@ class OrderTestCase(TestCase):
         price = 10,
         time = 123
       )
+      self.user = user
+      self.factory = RequestFactory()
 
     def test_status(self):
       order = Order.objects.get(pk=1)
       status = order.status
       self.assertEqual(status, 0)
+
+    def test_get_order(self):
+      request = self.factory.post('/api/authenticate/',{
+        'phone_number': '13230037688',
+        'verification_code': '0'
+      })
+      request.user = self.user
+      response = self.client.post('/api/listorders/')
+      self.assertEqual(response.status_code, 200)
+
 
