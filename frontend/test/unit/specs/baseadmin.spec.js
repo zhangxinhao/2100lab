62e6d42f755fa8386bdcf942d40925db30eccc75
刘彦秀 <lyx16280@163.com>
2018-08-29 21:51:27 +0800

    Add frontend test for admin pages. Ref #330

--- frontend/test/unit/specs/baseadmin.spec.js
+++ frontend/test/unit/specs/baseadmin.spec.js
@@ -1,5 +1,6 @@
 import baseAdmin from '@/components/admin/baseadmin'
 import ElementUI from 'element-ui'
+import { mount } from '@vue/test-utils'
 import Vue from 'vue'
 Vue.use(ElementUI)
 
@@ -12,5 +13,10 @@ describe('baseAdmin', () => {
     expect(typeof baseAdmin.data).toBe('function')
     const defaultData = baseAdmin.data()
     expect(typeof defaultData.authorizationList).toBe('object')
+  })
+  it('数目显示', () => {
+    const wrapper = mount(baseAdmin)
+    let inputs = wrapper.findAll('h1')
+    expect(inputs.length).toBe(0)
   })
 })
