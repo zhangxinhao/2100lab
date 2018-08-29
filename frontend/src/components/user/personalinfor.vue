3c715cea1f2d4f3de1842e6a288462b01d16152a
丁一峰 <879223669@qq.com>
2018-08-29 18:40:02 +0800

    Delete spare message. Ref #326

--- frontend/src/components/user/personalinfor.vue
+++ frontend/src/components/user/personalinfor.vue
@@ -104,7 +104,6 @@ export default {
       this.imageUrl = URL.createObjectURL(file.raw)
     },
     beforeAvatarUpload: function(file) {
-      console.log(file)
       const isJPG = file.type === 'image/jpeg'
       const isLt2M = file.size / 1024 / 1024 < 2
 
